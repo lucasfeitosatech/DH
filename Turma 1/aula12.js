beaeb59ed3a61e5706dc9c28aec0da540aba01ff
Lucas Feitosa <l.feitosa@reply.com>
2021-09-01 19:23:29 -0300

    Resolucao mesa de trabalho da aula 11

---
+++ Turma 1/aula12.js
@@ -0,0 +1,29 @@
+let peliculas = ["star wars", "totoro",  "rocky", "pulp fiction",  "la vida es bella"];
+
+function convertirAMayusculas(array){
+  array[0] = array[0].toUpperCase()
+  array[1] = array[1].toUpperCase()
+  array[2] = array[2].toUpperCase()
+  array[3] = array[3].toUpperCase()
+  array[4] = array[4].toUpperCase()
+  return array
+}
+
+function pasajeDeElementos(array1, array2) { 
+  array1.push(array2.pop().  toUpperCase()) 
+  array1.push(array2.pop(). toUpperCase()) 
+  array1.push(array2.pop(). toUpperCase()) 
+  array1.push(array2.pop(). toUpperCase()) 
+  array1.push(array2.pop(). toUpperCase()) 
+ }
+
+ const asiaScores = [8, 10, 6, 9, 10, 6, 6, 8, 4];
+const euroScores = [8, 10, 6, 8, 10, 6, 7, 9, 5]; 
+
+function compararCalificaciones(asia, europa) {
+  let comparacionesAsiaEuropa = []
+  comparacionesAsiaEuropa[0] = asia[0] === europa[0]
+  comparacionesAsiaEuropa[1] = asia[1] === europa[1]
+  comparacionesAsiaEuropa[2] = asia[2] === europa[2]
+  comparacionesAsiaEuropa[3] = asia[3] === europa[3]
+}
