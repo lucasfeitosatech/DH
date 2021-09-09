bd7ead64fc3d1d77377ffdbc9c1d6bd63db26f1a
Lucas Feitosa <l.feitosa@reply.com>
2021-09-08 22:10:29 -0300

    aula 13

--- Turma 1/aula13.js
+++ Turma 1/aula13.js
@@ -18,23 +18,21 @@ let c = [7, 5, 10, 8, 3];
 
 function pontuacaoMedia(array) {
   let soma = 0;
-  for(let i = 0;i<array.length; i++){
+  for (let i = 0; i < array.length; i++) {
     soma += array[i];
   }
-  return soma/array.length;
+  return soma / array.length;
 }
-
-
 
 /*
 3. Crie uma função pontuacaoMaior que receba um participante 
 por parâmetro e deve calcular e 
 devolver a pontuação mais alta que o participante tem.
 */
-function pontuacaoMaior(array){
+function pontuacaoMaior(array) {
   let maior = array[0];
-  for(let i = 1;i<array.length;i++){
-    if(array[i] > maior){
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] > maior) {
       maior = array[i];
     }
   }
@@ -52,7 +50,7 @@ anteriormente para calcular as médias e pontuações mais altas de cada uma,
 
 */
 
-function competicao(a,b,c){
+function competicao(a, b, c) {
   let mediaA = pontuacaoMedia(a);
   let maiorA = pontuacaoMaior(a);
 
@@ -62,28 +60,125 @@ function competicao(a,b,c){
   let mediaC = pontuacaoMedia(c);
   let maiorC = pontuacaoMaior(c);
 
-  if(mediaA > mediaB && mediaA > mediaC){
+  if (mediaA > mediaB && mediaA > mediaC) {
     console.log("Na media o participante A Ganhou!");
-  } else if (mediaB > mediaA && mediaB > mediaC){
+  } else if (mediaB > mediaA && mediaB > mediaC) {
     console.log("Na media o participante B Ganhou!");
-  } else if (mediaC > mediaA && mediaC > mediaB ){
+  } else if (mediaC > mediaA && mediaC > mediaB) {
     console.log("Na media o participante C Ganhou!");
   } else {
     console.log("Não houve vencedor nesta modalidade (Empate)");
   }
 
-  if(maiorA > maiorB && maiorA > maiorC){
+  if (maiorA > maiorB && maiorA > maiorC) {
     console.log("No maior o participante A Ganhou!");
-  } else if (maiorB > maiorA && maiorB > maiorC){
+  } else if (maiorB > maiorA && maiorB > maiorC) {
     console.log("No maior o participante B Ganhou!");
-  } else if (maiorC > maiorA && maiorC > maiorB ){
+  } else if (maiorC > maiorA && maiorC > maiorB) {
     console.log("No maior o participante C Ganhou!");
   } else {
     console.log("Não houve vencedor nesta modalidade (Empate)");
   }
+}
+
+//competicao(a,a,a);
+// Se a[i] > b[i], então Alice recebe 1 ponto.
+// 	Se a[i] < b[i], então Bob recebe 1 ponto.
+// 	Se a[i] = b[i], nenhuma pessoa recebe um ponto.
+
+
+const alicia = [23, 69, 32];
+const bob = [12, 70, 43];
+
+
+function Participante(mNome,mNotas){
+  this.nome = mNome;
+  this.notas = mNotas;
+}
+
+const participante1 = new Participante("Alicia",[23, 69, 32]);
+const participante2 = new Participante("Lucas",[12, 70, 43]);
+const participante3 = new Participante("Lucas",[12, 70, 43]);
+const participante3 = new Participante("Lucas",[12, 70, 43]);
+
+const alunos = [
+  {
+    id:1,
+    nome:"Lucas",
+    matricula:123,
+  },
+  {
+    id:2,
+    nome:"Lucas",
+    matricula:123,
+  },
+  {
+    id:1,
+    nome:"Lucas",
+    matricula:123,
+  }
+  ,{
+    id:1,
+    nome:"Lucas",
+    matricula:123,
+  }
+]
+
+
+
+
+function encontrarGanhador(participante1, participante2) {
+  //solução
+
+  let pontosA = 0;
+  let pontosB = 0;
+
+  for(let i = 0; i < participante1.notas.length ; i++){
+    if(participante1.notas[i] > participante2.notas[i]) pontosA++;
+    if(participante2.notas[i] > participante1.notas[i]) pontosB++;
+  }
+
+  if(pontosA > pontosB){
+    console.log(participante1.nome + " ganhou!")
+  } else if(pontosB > pontosA){
+    console.log(participante2.nome + " ganhou!");
+  } else {
+    console.log("Empate!");
+  }
+}
+
+encontrarGanhador(participante1,participante2);
+
+const pessoa = {
+  nome:"Lucas",
+  sobrenome:"Feitosa",
+  idade:"26",
 
 }
 
-competicao(a,a,a);
-
-
+const pessoas = [
+  {
+    nome:"Lucas",
+    sobrenome:"Feitosa",
+    idade:"26",
+  
+  },
+  {
+    nome:"Lucas",
+    sobrenome:"Feitosa",
+    idade:"26",
+  
+  },
+  {
+    nome:"Lucas",
+    sobrenome:"Feitosa",
+    idade:"26",
+  
+  },
+  {
+    nome:"Lucas",
+    sobrenome:"Feitosa",
+    idade:"26",
+  
+  }
+]
---
+++ cp-correcao-turma1.js
@@ -0,0 +1,140 @@
+/* 
+
+GRUPO 4
+
+Nomes: Mauro Lucio Marques, 
+Veronica Rizzi, 
+Bruno Batista, 
+Ana Carolina Ribeiro, 
+Fernanda Brum,
+ Marcelo Nascimento
+
+https://github.com/VevaRizzi/ckeckpointProgramacaoImperativa/blob/master/menuMicroondas.js
+
+Solução muito boa e bem estruturada! Parabéns pela ideia da função.
+
+*/
+
+/* 
+
+  EQUIPE:
+  Alexandre Filho
+  Joel Jerrys
+  Henrrique Brumatti
+  Renato Caldara
+  Glaydson
+  Alcinelson
+  https://github.com/Alexandrehos/checkpoint2/blob/main/microondas.js
+
+  else if(tempoInserido >= tempoPadrao){
+        console.log("Prato pronto, bom apetite!!!")
+    }
+
+  Esse if deveria estar fora, pois independete do tempo o prato vai ficar pronto e o log deve ser exibido.
+
+
+*/
+
+
+/*
+Grupo 3
+Integrantes:
+    - Gabriel Bernardo
+    - Wagner Moreira
+    - Jonatas Zamperi
+    - José Neto
+    - Vinícius Serra
+
+    https://github.com/gabrielbssantos/ctd-programacao-imperativa-aula-9/blob/main/menu-microondas.js
+
+    Código ficou muito repetido nos cases, dado que as condições são as mesmas independente do alimento para a queima e kabum, poderia ter sido criada uma funcao ou utilizar variaveis para buscar o tempoPadrao e realizar as condicoes apos a escolha do alimento.
+
+*/
+
+
+
+/*
+Grupo: 05
+Integrantes: 
+Belicio Cardoso,
+ Marcio Silva, 
+ Fabio Augusto, 
+ Amanda Jesus, 
+ Lucas Bernardo e 
+ Felipe Cabuto
+https://github.com/felipecabuto/ProgImp-Checkpoint1/blob/main/ProgImp-Checkpoint1.js
+
+else {
+        console.log("Prato pronto, bom apetite")
+    }
+
+
+   Esse if deveria estar, pois independete do tempo o prato vai ficar pronto e o log deve ser exibido.
+
+*/
+
+
+/*
+//Room 8 - 
+Cleiton ; 
+Flavio; 
+Willian; 
+Rafael; 
+Aderson
+https://github.com/CBernsmuller/CTD---Programa-o-Imperativa-Turma-01-NT/blob/master/aula%2009%20-%2025-08/microondas.js
+
+Faltou criar uma função conforme solicitado e as condições ficaram muito repetidas, poderia ter sido criado uma variável tempoPadrao e fazer as validações de acordo com essa variável.
+
+*/
+
+
+/*
+## GRUPO 7 ##
+INTEGRANTES:
+> DAVI DUTRA
+> FELIPE ROCHA
+> FERNANDA LIMA
+> HARRY MÖBBS JÚNIOR
+> VINICIUS MACHADO RODRIGUES
+Endereço do git: https://github.com/harrymobbsjunior/Checkpoint.git
+
+else if (tempo >= padrao && tempo < (padrao * 2)) {
+        return "Prato pronto, bom apetite!!!";
+    }
+
+    Esse if deveria estar, pois independete do tempo o prato vai ficar pronto e o log deve ser exibido.
+
+*/
+
+/*
+Nome: Gustavo Henrique Muraro Monteiro
+
+Grupo 2: 
+Julio Campos, 
+Peterson Alves, 
+Ronan Junqueira, 
+David Franklin, 
+Luis Soares, 
+Israel
+
+Código ficou muito repetido nos cases, dado que as condições são as mesmas independente do alimento para a queima e kabum, poderia ter sido criada uma funcao ou utilizar variaveis para buscar o tempoPadrao e realizar as condicoes apos a escolha do alimento.
+
+*/
+
+/*
+
+// Turma: 01 NT
+// Integrantes: 
+Alisson Pinho, 
+Hiego Willy, 
+Jhonathan Weber, 
+Patricia Kushima, 
+Yasmin Lorraine.
+
+https://drive.google.com/file/d/1veS-7eXSMIVvgo7LObU2wLPs3uyEM-vs/view
+
+Código ficou muito repetido nos cases, dado que as condições são as mesmas independente do alimento para a queima e kabum, poderia ter sido criada uma funcao ou utilizar variaveis para buscar o tempoPadrao e realizar as condicoes apos a escolha do alimento.
+*/
+
+
+
