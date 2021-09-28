ec8ed863696389eb45f9653795949f52cf6c331a
Lucas Feitosa <l.feitosa@reply.com>
2021-09-27 22:08:07 -0300

    revisao

--- Turma 1/aula19/aula19.js
+++ Turma 1/aula19/aula19.js
@@ -2,7 +2,14 @@
 // sexo (M ou F) de 15 pessoas. 
 
 let meubanco = require("./meubanco");
-meubanco.maiorEMenorAltura();
+//console.log(meubanco.pessoas);
+//meubanco.maiorEMenorAltura();
+let media = meubanco.calcularMediaOtimo();
+console.log("Media da idade dos que responderam otimo: " + media.toFixed(2));
+let qtd = meubanco.qtdPessoasRegular();
+console.log("Quantidade de pessoas regular: " + qtd);
+let porcentagem = meubanco.porcentagemDosBons();
+console.log("A porcentagem dos bons é: " + porcentagem + "%");
 
 
 
--- Turma 1/aula19/meubanco.js
+++ Turma 1/aula19/meubanco.js
@@ -1,10 +1,12 @@
 // Tem-se um conjunto de dados contendo a altura e o 
 // sexo (M ou F) de 15 pessoas. 
 
-function Pessoa(altura_,sexo_,nome_){
+function Pessoa(altura_,sexo_,nome_,idade_,opiniao_){
   this.altura  = altura_
   this.sexo = sexo_
   this.nome = nome_
+  this.idade = idade_
+  this.opiniao = opiniao_
 }
 
 // let pessoa = {
@@ -12,7 +14,8 @@ function Pessoa(altura_,sexo_,nome_){
 //   sexo:'M'
 // }
 let array = [];
-for(let i = 1;i<=100;i++) {
+for(let i = 1;i<=111;i++) {
+  //Números aleatórios entre 0 e 1. 
   let numRandom = Math.random();
   let altura = numRandom + 1;
   let nome = 'Pessoa ' + i;
@@ -20,8 +23,20 @@ for(let i = 1;i<=100;i++) {
   if(numRandom > 0.5){
     sexo = 'M';
   }
+  let opiniao;
+  if(numRandom < 0.33){
+    opiniao = '3';
+  } else if(numRandom >= 0.33 && numRandom < 0.66){
+    opiniao = '2';
+  } else {
+    opiniao = '1';
+  }
+  let idade = Number((numRandom * 100 + 1).toFixed(0)); 
 
-  array.push(new Pessoa(Number(altura.toFixed(2)),sexo,nome));
+
+  //ótimo - 3, bom - 2, regular -1.
+
+  array.push(new Pessoa(Number(altura.toFixed(2)),sexo,nome,idade,opiniao));
 
 }
 
@@ -64,12 +79,76 @@ module.exports = {
   },
   //// a média de altura das mulheres;
   mediaAlturaMulheres:function(){
+    
+    let contadorMulheres = 0;
+    let somaDasAlturas = 0;
+    
+//     Pessoa { altura: 1.3, sexo: 'F', nome: 'Pessoa 46' }
+//      Pessoa { altura: 1.7, sexo: 'F', nome: 'Pessoa 70' }
+    for(let pessoa of this.pessoas){
+      if(pessoa.sexo === 'F'){
+        contadorMulheres = contadorMulheres + 1;
+        somaDasAlturas = somaDasAlturas + pessoa.altura;
+      }
+    }
+
+    const media = somaDasAlturas/contadorMulheres;
+    return media;
 
   },
   //o número de homens.
   qtdHomens:function(){
+    let contadorHomens = 0;
 
+    for(let pessoa of this.pessoas){
+
+      if(pessoa.sexo === 'M'){
+        contadorHomens = contadorHomens + 1;
+      }
+
+    }
+    return contadorHomens;
+  },
+
+  //a média das idades das pessoas que responderam ótimo;
+
+  calcularMediaOtimo:function(){
+    let contadorOtimo = 0;
+    let somaDasIdades = 0;
+
+    for(let pessoa of this.pessoas){
+      if(pessoa.opiniao == '3'){
+        contadorOtimo = contadorOtimo + 1;
+        somaDasIdades += pessoa.idade;
+      }
+    }
+
+    const media = somaDasIdades/contadorOtimo;
+    return media;
+
+  },
+// a quantidade de pessoas que responderam regular;
+  qtdPessoasRegular:function(){
+    let contadorRegular = 0;
+    for(let pessoa of this.pessoas){
+      if(pessoa.opiniao == '1'){
+        contadorRegular++;
+      }
+    }
+    return contadorRegular;
+  },
+// a porcentagem de pessoas que responderam bom entre todos os espectadores analisados.
+  porcentagemDosBons:function(){
+    let contadorBom = 0;
+    for(let pessoa of this.pessoas){
+      if(pessoa.opiniao == '2'){
+        contadorBom++;
+      }
+    }
+    let porcentagem = (contadorBom/this.pessoas.length)*100;
+    return porcentagem;
   }
+
 }
 
 
