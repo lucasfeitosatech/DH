37f73e986745be4651abfbcfb4067114f5811294
Lucas Feitosa <l.feitosa@reply.com>
2021-11-03 21:01:01 -0300

    Aula 07 - Turma 02

---
+++ Turma2/Aula7/index.js
@@ -0,0 +1,31 @@
+//Operadores lógicos 
+const desigualdade = 1 != 2;
+const igualdade = 'nome'.toLowerCase() == 'NOME'.toLowerCase();
+const maior = 5 > 3;
+const maiorIgual = 6 >= 3;
+const menor = 0 < 1;
+const menorIgual = 1<= 2;
+const verdadeiro = true;
+//negacao
+//console.log(!verdadeiro);
+
+
+let a = "Hoje vou dar aula".length;
+let b = "ou Hoje vai chover";
+
+
+let x = 5;
+
+let resultado = (x > 1 && x<10) || (x*2 > 2 && x*2 < 20);
+
+let nome = "";
+console.log(!nome);
+nome = "lucas";
+console.log(!nome);
+
+//false
+let numero = 0;
+//true
+numero = 1;
+
+
