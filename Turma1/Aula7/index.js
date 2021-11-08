02e9d135240efe2fe3aa29092fea566bceb15726
Lucas Feitosa <l.feitosa@reply.com>
2021-11-08 18:43:31 -0300

    aula 07, operadores logicos

---
+++ Turma1/Aula7/index.js
@@ -0,0 +1,17 @@
+//Operadores lógicos
+
+const igualdade = 'lucas'.toLowerCase() === 'LUCAS'.toLowerCase(); //lucas;
+const desigualdade = 2 !== '3';
+const maior = 1 > 2;
+const menor = 1 < 2;
+const maiorIgual = 1 >= 2;
+const menorIgual = 5 <= 10;
+const negacao = true;
+const ativo = false;
+
+let a = "Hoje vou ao cinema";
+let b = "OU Hoje vai chover";
+
+let or = false || false ;
+console.log(or);
+
