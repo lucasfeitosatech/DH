7919681d12e4aef3e9cefa5377185e23750feb1d
Lucas Feitosa <l.feitosa@reply.com>
2022-02-14 15:56:34 -0300

    Aula 4

---
+++ aula2/ex1.js
@@ -0,0 +1,65 @@
+
+// Ctrl + /
+//Declaração de variável
+
+//Alt + Shift + A
+/*
+dhasuihdisahdiuas
+dhiaushdui38129083021
+371289398123
+739217398213
+*/
+
+//Tipos básicos
+const minhaString = 'Olá mundo';
+const numero = 5;
+const flutuante = 5.23123213213;
+const verdadeiro = true;
+const falso = false;
+const nulo = null;
+const infinito = Infinity;
+
+//Operadores Matemáticos
+const soma = 2 + 5;
+const subtracao = 2 - 5;
+const divisao = 5 / 2;
+const multiplicao = 3*5;
+const mod = 5 % 2;
+const exponencial = 2**2;
+
+//Operadores lógicos 
+
+const maior = 2 > 3; //false
+const maiorIgual = 2 >= 2; // true
+const menor = 2 < 3; // true
+const menorIgual = 2 <= 3; // true
+//Este operadora compara apenas o valor das variável
+const igualdadeSimples = 2 == '2';//true
+//Este operador compara o valor e o tipo da variável 
+const igualdadeTripla = 2 === '2';//False
+//Diferente
+
+const desigualdadeSimples = 2 != '2';//false ! =
+//Diferente
+const desigualdadeTripla = 2 !== '2'; // ! = =
+
+//O Let e o var permite a troca do valor da variável
+
+if(true){
+  var contador = 0;
+  // o let apenas vai existir no escopo o qual ele foi declarado
+  let numero1 = 5;
+  numero1 = 10;
+  // console.log(numero1);
+}
+
+console.log(numero4);
+
+var contador = 1;
+contador = contador + 1;
+
+// console.log(contador);
+
+
+
+
