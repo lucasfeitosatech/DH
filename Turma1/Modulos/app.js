58f05c7ddefa7380b7a92e61a20d472a408754e6
Lucas Feitosa <l.feitosa@reply.com>
2022-02-14 14:07:56 -0300

    Limpando pasta

--- Turma1/Modulos/app.js
+++
@@ -1,6 +0,0 @@
-let grupo = require('./grupo');
-console.log(grupo.pessoas);
-grupo.novaPessoa('Pessoa6',1.65,'H');
-grupo.novaPessoa('Pessoa7',1.45,'H');
-console.log(grupo.pessoas);
-grupo.maiorEmenorAltura();
