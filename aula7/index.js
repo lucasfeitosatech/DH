0d63bd2792e5bce9036948cb8ced186d0d0e6db7
Lucas Feitosa <l.feitosa@reply.com>
2022-02-21 15:51:20 -0300

    Aula 07

---
+++ aula7/index.js
@@ -0,0 +1,55 @@
+//Maior e menor igual
+4 >= 3 // true  
+3 > 4;
+
+3 <= 4
+4 < 3 // true
+//Igualdade 
+//Analisa apenas o valor
+'4' == 4 // true 
+// Analisa o valor e o tipo
+'4' === 4 // false
+
+'4'!= 4 // false
+
+'4' !== 4 // true
+
+
+const expresao1 = "Vou estudar"
+const expressao2 = "Hoje vai chover";
+
+//Hoje vou estudar e hoje vai chover
+
+//AND => E -> && 
+
+console.log(1 < 2 && 2 > 4 && 0 == '0' && 2 < 3 && 8 == 8)
+
+//OR -> OU -> ||
+//Hoje vou estudar ou hoje vai chover 
+
+console.log(!(1 < 2 || 2 > 4))//false
+
+const nome = "Lucas";
+nome.length > 2 && nome.length < 15;//Nome válido
+
+//! Negação 
+
+console.log("" && 6)// false; 
+console.log(!false)// true;
+
+//Thruty and Falshy 
+
+"" //false
+"Minha string"//true
+0 // false
+1// true
+undefined // false
+null // false
+
+
+
+
+
+
+
+
