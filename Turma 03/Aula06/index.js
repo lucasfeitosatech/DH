b82f598c434819fcb9a61a896fce74c37824429f
Lucas Feitosa <l.feitosa@reply.com>
2022-03-14 14:09:28 -0300

    Aula 06

---
+++ Turma 03/Aula06/index.js
@@ -0,0 +1,36 @@
+const id = "abcdf123!@#";
+let contador = 0;
+contador++;//1
+contador++;//2
+console.log(contador);
+
+//Declaração de função
+function minhaFuncao() {
+  console.log("Minha função");
+}
+//Execução da função
+// minhaFuncao();
+// minhaFuncao();
+// minhaFuncao();
+
+// function soma(num1,num2){
+//   return num1 + num2;
+//   let x = num1 + num2;
+// }
+console.log(soma2(2,4));
+//Tradicional - Possui escopo global
+function soma(num1,num2,num3){
+  return num1 + num2;
+}
+
+function dividir(num1,num2){
+  return num1 / num2;
+}
+
+
+
+function mediaDeTres(num1,num2,num3){
+
+  return (num1)
+}
+
