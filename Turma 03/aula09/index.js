4ae5cb048c4a12035b9e56c9394154e6991ecdf3
Lucas Feitosa <l.feitosa@reply.com>
2022-04-12 15:52:54 -0300

    aula html

--- Turma 03/aula09/index.js
+++
@@ -1,8 +0,0 @@
-function microondas(opcao,tempo) {
-
-}
-
-microondas(20,5);
-
-console.log("Opção inválida");
-console.log("Tempo insuficiente");
--- index.js
+++ index.js
@@ -1,16 +1,31 @@
-let aluno = {
-  calcularMedia: function(){
+window.onload = function(){
+  console.log(document);
+  const h1 = document.getElementById('title');
+  h1.innerHTML = "Texto trocado no JS";
+  console.log(h1);
+}
 
+
+function aoClicarNoBotao(){
+  const h1 = document.getElementById('title');
+  const input = document.getElementById('texto');
+  h1.innerHTML = `${input.value}`
+}
+
+function aoTrocarTextoDoInput(){
+  const valorDoInput = event.target.value;
+  if(!valorDoInput.includes("@")){
+    document.getElementById('texto').style.backgroundColor = 'red';
+  } else {
+    document.getElementById('texto').style.backgroundColor = 'transparent';
   }
 }
 
-let media = function(){
-  if(true){
-    
-  }
-  else{
-    return 
+function aoTrocarSenha(){
+  const valorDoInput = event.target.value;
+  if(valorDoInput.length < 5){
+    document.getElementById('senha').style.backgroundColor = 'red';
+  } else {
+    document.getElementById('senha').style.backgroundColor = 'transparent';
   }
 }
-
-console.log(media());
