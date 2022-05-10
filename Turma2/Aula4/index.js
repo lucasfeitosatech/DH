318194e58d64df0b9611809731c4612020287b42
Lucas Feitosa <l.feitosa@reply.com>
2022-05-09 22:32:34 -0300

    Aula 4

--- Turma2/Aula4/index.js
+++ Turma2/Aula4/index.js
@@ -1,8 +1,102 @@
 // NodeJS -> Interpretador de código Javascript 
 // VSCode -> Editor de texto ofical. 
+// console.log() => Imprime na tela o nós colocarmos entre os (parenteses)
+/* console.log() => É uma função e tudo que 
+ estiver dentro dos (...) são consdierados parametros da função
+ Parametros são separados por ',' */
 
-console.log("Hello World");
-console.log("Testando codigo no vim");
-console.log("Testando meu código no editor de texto do Mac");
+//console.log("Hello World","Testando codigo no vim","Testando meu código no editor de texto do Mac");
+// Declarando uma variavel
+let nome;
+// undefined -> Não definido
+
+// Fazendo uma atribuição a variavel
+// = -> atribuição 
+let sobrenome = 'Ciclano';
+var c;
+
+console.log(nome);
+console.log(sobrenome);
+
+/*
+  const 
+
+  Sempre temos que inicializar o const com algum valor; 
+  uma vez que o valor foi definido, o mesmo não pode ser alterado
+
+*/
+// 1 segundo = 1000ms
+const segundo = 1000;
+const minuto = 60 * segundo;
+const hora = 60 * minuto;
+
+/*
+
+  let,var
+
+  uma vez que o let foi definido seu valor pode ser alterado
+*/
+
+
+let numero = 5;
+numero = 10;
+
+console.log(x);
+var x = 8;
+x = 12;
+console.log(x);
+
+{
+  var bloco = '';
+}
+
+console.log(bloco);
+
+/*
+
+*/
+
+// 5/2 -> 2*2 + 1 // 1 = resto
+
+// Ctrl + D -> Seleciona todas as referencias e as altera
+const soma = 5 + 8;
+const subtrair = 3 - 2;
+const multiplicar = 3 * 2;
+const divisao = 5 / 2 // Divisão exata 2.5
+const divisaoModular = 5 % 2 // 1
+const exponenciao = 2 ** 4 // 2*2*2*2 
+const raizQuadrado = 4 ** 0.5// Hack da raiz da quadrada
+const raizQudrada = Math.sqrt(4); // Raiz usando Math
+const pow = Math.pow(2,4); // Exponenciao usando Math
+
+// Concatenação -> Junta os dois textos 
+console.log("Hello World" + " Teste"); // limite x -> 0 = infinito
+
+
+
+// const x = 5; const y = 8
+
+console.log(raizQuadrado);
+
+
+// Diferença entre let e var 
+/*
+
+var -> Escopo Global
+let -> Escopo local ou de bloco
+
+*/
+
+
+
+
+
+
+
+
+
+// Variavel -> Armazenamento de alguma informação me memória
+// let,var, const 
+
 
 
