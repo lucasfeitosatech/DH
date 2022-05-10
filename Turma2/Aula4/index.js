2b159c344d23bf7197c82ee3777c335c0fd4cc73
Lucas Feitosa <l.feitosa@reply.com>
2022-05-09 21:26:51 -0300

    git lens

---
+++ Turma2/Aula4/index.js
@@ -0,0 +1,8 @@
+// NodeJS -> Interpretador de código Javascript 
+// VSCode -> Editor de texto ofical. 
+
+console.log("Hello World");
+console.log("Testando codigo no vim");
+console.log("Testando meu código no editor de texto do Mac");
+
+
