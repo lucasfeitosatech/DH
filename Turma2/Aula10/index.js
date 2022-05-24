4ebe32720627760f7c00ab47d21f47b11d74c420
Lucas Feitosa <l.feitosa@reply.com>
2022-05-23 22:51:34 -0300

    AUla 10

--- Turma2/Aula10/index.js
+++ Turma2/Aula10/index.js
@@ -12,16 +12,36 @@
   ações dentro do nosso objeto
 
 */
-
-let readlineSync = require('readline-sync');
-
+//           0123456789...
+// M -> 0
+// I -> 1
+// N -> 2
+//...
+// G -> 11
+let texto = "MINHA STRING";
 // ToUpperCase() -> Converter a minha string para letras maisculas
+// ToLowerCase() -> Converter a string para letras minusculas
 console.log(texto.toLowerCase());
 
-// let senha = "1234";
-// if(senha.length < 4){
-//   console.log("Por favor, preencha uma senha com mais de 4 caracteres")
-// } else {
-//   console.log("Senha válida");
-// }
+// includes() -> Verifica se o texto especificado entre () existe na string
+let email = "teste@teste.com";
+if(email.includes("@") && email.includes(".com")){
+  console.log("É um email válido");
+} else {
+  console.log("É um email inválido");
+}
+
+
+
+let senha = "1234";
+if(senha.length < 4){
+  console.log("Por favor, preencha uma senha com mais de 4 caracteres")
+} else {
+  console.log("Senha válida");
+}
+
+let texto2 = "teste a minha string & teste ooutro & & & & & & & & &"
+console.log( texto2.replace('&','E comercial') );
+
+
 
