01399fc1dfc0d89f73196c16f4f4127933842553
Lucas Feitosa <l.feitosa@reply.com>
2022-08-03 19:14:08 -0300

    Aula 02

---
+++ Aula02/index.js
@@ -0,0 +1,28 @@
+// Aula 02 
+/* Linguagem e comunicação 
+
+  - Lighbot -> Joguinho que utiliza uma linguagem simbólica
+  - Mumuki -> Linguagem textual 
+
+  - Idiomas, fala 
+  - Comunição entre pessoas
+    - Fala
+    - Libras
+
+  - Cetáceos -> Ondas sonoras 
+
+  - Máscara de CNPJ 
+  
+
+  
+
+
+
+
+*/
+
+console.log('');
+// se algo for verdadeiro entao faca alguma coisa
+if(true){
+
+}
