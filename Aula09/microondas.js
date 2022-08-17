1d67a678e6399f7abc5e128f727b8a0fb00a93b7
Lucas Feitosa <l.feitosa@reply.com>
2022-08-17 19:14:54 -0300

    aula 09

---
+++ Aula09/microondas.js
@@ -0,0 +1,23 @@
+// const readlineSync = require('readline-sync');
+
+// console.log(`
+//   Bem-vindo ao X-podrão 
+
+//   Escolhe uma dentre as opções
+
+//   01 - X-Bancon
+//   02 - X-Salada
+//   03 - X-Egg
+
+// `);
+
+// readlineSync.question('Qual a sua opção?')
+// readlineSync.question('Qual o tempo de execução')
+
+
+function microondas(opcao,tempo){
+  // QUeima
+  
+}
+
+microondas(1,60)
