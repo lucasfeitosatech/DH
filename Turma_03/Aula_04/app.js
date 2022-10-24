a336d345a60a4dbed561f9da5013271c7b7dada6
Lucas Feitosa <l.feitosa@reply.com>
2022-10-24 20:44:52 -0300

    Aula 04

---
+++ Turma_03/Aula_04/app.js
@@ -0,0 +1,42 @@
+
+
+// Diferença entre let e var;
+
+console.log(minhaVariavel); // undefined
+// Declaração de variável;
+var minhaVariavel = 5;
+console.log(minhaVariavel); // 5
+// Reatribuindo a variavável
+minhaVariavel = 20;
+
+
+// Visibilidade Global
+
+
+minhaVariavel = 12;
+console.log(minhaVariavel); // 12
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//var minhaVariavel;// -> undefined -> Não definido
+
