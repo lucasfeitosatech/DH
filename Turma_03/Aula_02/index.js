60628ff27bc396d322b6b32521241a0007da3fc3
Lucas Feitosa <l.feitosa@reply.com>
2022-10-24 20:44:59 -0300

    Aula 04

---
+++ Turma_03/Aula_02/index.js
@@ -0,0 +1,13 @@
+// Aula 2
+
+/*
+  LightBot -> É um joguinho que utiliza uma plataforma gráfica para rodar instruções
+  em um pequeno rôbo para completar um objetivo. Utiliza símbolos para executar as ações
+
+  Mumuki -> Rodar através de códigos
+
+
+
+
+
+*/
