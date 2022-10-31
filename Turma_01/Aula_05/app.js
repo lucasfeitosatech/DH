d60ad5d3c69663ed9d8d20bda33c6f8d631ead90
Lucas Feitosa <l.feitosa@reply.com>
2022-10-31 19:08:16 -0300

    Continuacao funcoes

--- Turma_01/Aula_05/app.js
+++ Turma_01/Aula_05/app.js
@@ -29,17 +29,70 @@
 
 */
 // Declaração
+//calculoImc('Carlos de Souza',28,1.76,80.1);
+//calculoImc('Aline Ferreira',33,1.53,63.7);
 function calculoImc(nome,idade,altura,peso){
   const imc = peso/(altura**2);
   console.log(nome + ' tem ' + idade + ' anos e seu índice de massa corporal é de ' + imc.toFixed(2));
 }
+// Execução/chamada/invocação da função
 
-calculoImc('Carlos de Souza',28,1.76,80.1);
-calculoImc('Aline Ferreira',33,1.53,63.7);
+/*
+  Função expressa -> Uma forma de atribuir a função a uma variável
+*/
+
+const numero = 5;
+const texto = 'Meu Texto';
+const temPlano = true;
+// Função expressa
+                      // Função anonima
+// minhaFuncao()  // -> ERRO  
+let minhaFuncao = function(param1,param2){
+
+
+}
+
+
 
 
 /*
-  funçaao expressa
+  Continuação Aula 05 -
+
+  Funções possumem comportamento de forma similar ao var, ou seja,
+  possuem visibilidade global
+  
   valores padroes dos parametros
   retornos
 */
+
+function soma(num1 = 0,num2 = 0,num3 = 0){
+  /*
+    num1 -> 0
+    num2 -> 0
+    num3 -> 0
+  */
+
+  /*
+    palavra reservada
+    return -> Vai especificar o que vai ser retornado da função
+    ou seja, na hora da execução da função esse valor vai ser devolvido
+
+    termina a função 
+  */
+ return num1 + num2 + num3;
+ 2**2
+ Math.pow(2,2);
+}
+
+/*
+  Parametros -> Os valores de entrada
+  Após o processamento da função 
+  A execução da mesma sempre vai esperar por algum valor de saída
+  caso a função não tenha um retorno definido ela sempre retornará o valor
+  undefined
+*/
+
+// Toda chamda de função sempre espera por algum valor de saída
+const retorno = soma(1,2,3);
+calculoImc(retorno);
+console.log(retorno);
