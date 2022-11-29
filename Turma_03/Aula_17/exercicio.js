2373f0f75682bb23bf68fe0b9e8afb2ec6a1fe62
Lucas Feitosa <l.feitosa@reply.com>
2022-11-29 19:19:19 -0300

    Aula 17

---
+++ Turma_03/Aula_17/exercicio.js
@@ -0,0 +1,31 @@
+const conta = {
+  //... propriedades
+}
+
+
+// Número da conta (somente números)
+// Tipo de conta (conta corrente ou poupança em $)
+// Saldo em $ (valor apenas)
+// Titular da conta (nome completo)
+
+function Conta(conta,tipo,saldo,titular){
+  // ... criar a funcão construtura
+}
+
+class Conta {
+  constructor(conta,tipo,saldo,titular){
+// ... montagem do objeto
+  }
+
+  // metódos...
+}
+
+const banco = {
+  clientes:[],
+  adicionarCliente(conta,tipo,saldo,titular){
+    const novaConta = new Conta()
+    // ...
+  }
+}
+
+
