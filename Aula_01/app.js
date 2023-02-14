e1fac437254ab99b484ab2ca65ab56dd7d9f3e2a
Lucas Feitosa <l.feitosa@reply.com>
2023-02-13 22:07:53 -0300

    Aula 01

---
+++ Aula_01/app.js
@@ -0,0 +1,22 @@
+// 1 - Pensamento computacional 
+/*
+  O que é? 
+    R: Uma forma de resolver problemas utilizando algumas técnicas 
+  
+  Problema: Lavar a louça
+  
+  Decomposição: Quebrar o nosso problema em etapas menores (ou em pequenos problemas)
+  Abstração: Focar apenas no que é importante, deixa de lado que não é essencial
+  Identificar padrões: Buscar por coisas que se assemelham 
+  Algoritmo: Passo a passo para a resolução, as etapas sequencias para a resolução
+
+*/
+// -> Comentário de uma linha 
+/*  
+  Tudo que estiver dentro
+
+  é um comentário
+
+*/
+
+1 + 2
