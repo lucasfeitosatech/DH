a7d5dd859afa40fa00f000135e0f86383eabc9bd
Lucas Feitosa <l.feitosa@reply.com>
2023-02-13 21:38:49 -0300

    Aula 01

---
+++ Aula_01/index.js
@@ -0,0 +1,26 @@
+// -> Comentário de uma linha
+
+/*
+  Aula 01 - Pensamento computacional
+
+  Aulas: Seg,Ter,Qua -> 21:00 -> 22:45 
+
+  Avaliações: 
+    - Checkpoint 1 -> Prático (código)
+    - Checkpoint 2 -> Prova (forms) -> Subjetivo (Multipla escolha)
+    - Checkpoint Final (3) -> Trabalho final -> Prático (código)
+
+    Composição da nota: 
+
+    10% -> Participação -> Consumo do PG, mesas de trabalho e afins 
+    40% -> Média do CP1 + CP2 
+    50% -> CP3 
+
+  Ferramentas: 
+
+    - VSCode -> Editor de texto com superpoderes 
+    - NodeJS -> Para rodar os nossos códigos 
+    - Git -> 
+    - Discord -> Canal ofical de comunição
+
+*/
