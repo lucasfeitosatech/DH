23eab1eb0357428ca3e261f95c18b84dba235bf4
Lucas Feitosa <l.feitosa@reply.com>
2023-03-06 22:00:14 -0300

    Calculadora

---
+++ Aula_06/calculadora.js
@@ -0,0 +1,58 @@
+/*
+  Calculadora - Nível I
+*/
+
+function soma(num1,num2){
+  return num1 + num2;
+}
+
+function subtracao(num1,num2){
+  return num1 - num2;
+}
+
+function divisao(num1,num2){
+  return num1 / num2;
+}
+function multiplicacao(num1,num2){
+  return num1 * num2;
+}
+
+/*
+  Calculadora - Nível II
+*/
+
+console.log ("-------------- Teste de Operações / Calculadora --------------");
+console.log('Soma: ' , soma(5,6));
+
+
+/*
+Calculadora - Nível III - Funções Extras
+*/
+
+function quadradoDoNumero(n){
+  // return n*n;
+  const quadrado = multiplicacao(n,n);
+  return quadrado
+}
+
+function mediaDeTresNumeros(num1,num2,num3){
+  // return (num1+num2+num3)/3;
+  const soma1 = soma(num1,num2);
+  const soma2 = soma(soma1,num3);
+  const resultado = divisao(soma2,3);
+  return resultado;
+
+  // return divisao(soma(num1,soma(num2,num3)),3);
+}
+
+function calculaPorcentagem(numero,porcentagem){
+  // return numero*(porcentagem/100);
+  const percentual = divisao(porcentagem,100);
+  return multiplicacao(numero,percentual);
+}
+
+function geradorDePorcentagem(valorPercentual,valor){
+  // return (valorPercentual/valor)*100;
+  const percentual = divisao(valorPercentual,valor);
+  return multiplicacao(percentual,100);
+}
