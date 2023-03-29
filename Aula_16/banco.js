89448df22b9c1cdaf5113e7289c19531b68c0d8a
Lucas Feitosa <l.feitosa@reply.com>
2023-03-28 21:40:21 -0300

    Aula 16

--- Aula_16/banco.js
+++ Aula_16/banco.js
@@ -2,6 +2,7 @@
 
 class Conta {
   constructor(numero,tipo,saldo,titular){
+    // this -> {} -> objeto vazio
     this.numero = numero
     this.tipo = tipo;
     this.saldo = saldo
@@ -18,23 +19,43 @@ class Conta {
 const banco = {
   clientes:[],
   adicionarCliente(numero,tipo,saldo,titular){
+    //            new -> novo 
     const conta = new Conta(numero,tipo,saldo,titular)
+    // this vai ser a referência do objeto o qual estamos trabalhando
+    // this é igual ao banco
     this.clientes.push(conta);
   },
-  consultarCliente(){
-
+  consultarCliente(titularASerConsultado){
     /*
     6 - o objeto do banco criará um método chamado consultarCliente que receberá
      um nome (titular) por parâmetro e deve pesquisar na lista de contas e 
      retornar ao objeto do cliente que corresponde a esse nome inserido.
 
     */
-    
+
+     // titularASerConsultado = 'Lucas'
+    for(let cliente of this.clientes){
+      if(cliente.titular === titularASerConsultado){
+        return cliente;
+      }
+    }
+    return null;    
   }
 }
 banco.adicionarCliente('01','conta corrente',0,'Fulano 1');
 banco.adicionarCliente('02','conta corrente',0,'Fulano 2');
-console.table(banco.clientes);
+banco.adicionarCliente('03','conta corrente',0,'Fulano 3');
+banco.adicionarCliente('04','conta corrente',0,'Lucas');
+const clienteEncontrado = banco.consultarCliente('Fulano 1');
+console.log(clienteEncontrado);
+
+//console.table(banco.clientes);
+
+// for of 
+
+
+
+
 // const conta1 = new Conta('01','conta corrente',0,'Fulano 1');
 // const conta2 = new Conta('02','conta poupança',0,'Fulano 2');
 // const conta3 = new Conta('03','conta corrente',0,'Fulano 3');
