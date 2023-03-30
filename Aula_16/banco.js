84b835e3b5cdc871e19ddc68c69a92ff98ceaa65
Lucas Feitosa <l.feitosa@reply.com>
2023-03-29 21:10:40 -0300

    Deposito e Saque Exercicio banco

--- Aula_16/banco.js
+++ Aula_16/banco.js
@@ -1,61 +1,59 @@
-// 1 e 3 - 
+// 1 e 3 -
 
 class Conta {
   /*
   Um modelo de criação dos objetos 
 
   */
-  constructor(numero,tipo,saldo,titular){
+  constructor(numero, tipo, saldo, titular) {
     // this -> {} -> objeto vazio
-    this.numero = numero
+    this.numero = numero;
     this.tipo = tipo;
-    this.saldo = saldo
-    this.titular = titular
+    this.saldo = saldo;
+    this.titular = titular;
   }
 }
 
-// 2 e 4- 
-class Banco {
-  constructor(nome){
-    this.clientes = [];
-    this.nome = nome;
-  }
+// 2 e 4-
+// class Banco {
+//   constructor(nome){
+//     this.clientes = [];
+//     this.nome = nome;
+//   }
 
-  consultarCliente(titularASerConsultado) {
-    for(let cliente of this.clientes){
-      if(cliente.titular === titularASerConsultado){
-        return cliente;
-      }
-    }
-    return null; 
-  }
-  
-  metodo2(){
+//   consultarCliente(titularASerConsultado) {
+//     for(let cliente of this.clientes){
+//       if(cliente.titular === titularASerConsultado){
+//         return cliente;
+//       }
+//     }
+//     return null;
+//   }
 
-  }
+//   metodo2(){
 
-  metodo3(){
-    
-  }
-}
+//   }
 
-const santander = new Banco('Santander');
-santander.consultarCliente()
+//   metodo3(){
 
+//   }
+// }
 
+// const santander = new Banco('Santander');
+// santander.consultarCliente()
 
-// 5 
+// 5
 
 const banco = {
-  clientes:[],
-  adicionarCliente(numero,tipo,saldo,titular){
-    //            new -> novo 
-    const conta = new Conta(numero,tipo,saldo,titular)
+  clientes: [],
+  adicionarCliente(numero, tipo, saldo, titular) {
+    //            new -> novo
+    const conta = new Conta(numero, tipo, saldo, titular);
     // this vai ser a referência do objeto o qual estamos trabalhando
     // this é igual ao banco
     this.clientes.push(conta);
   },
-  consultarCliente(titularASerConsultado){
+  consultarCliente(titularASerConsultado) {
     /*
     6 - o objeto do banco criará um método chamado consultarCliente que receberá
      um nome (titular) por parâmetro e deve pesquisar na lista de contas e 
@@ -63,37 +61,52 @@ const banco = {
 
     */
 
-     // titularASerConsultado = 'Lucas'
-    for(let cliente of this.clientes){
-      if(cliente.titular === titularASerConsultado){
+    // titularASerConsultado = 'Lucas'
+    for (let cliente of this.clientes) {
+      if (cliente.titular === titularASerConsultado) {
         return cliente;
       }
     }
-    return null;    
+    return null;
   },
-  deposito(titular,valorDeposito){
-
-
+  deposito(titular, valorDeposito) {
+    const cliente = this.consultarCliente(titular);
+    if (cliente !== null) {
+      cliente.saldo += valorDeposito;
+      console.log("Seu novo saldo é de: R$ " + cliente.saldo.toFixed(2));
+    } else {
+      console.log("Não foi possível realizar o depósito");
+    }
   },
-  listarClientes(){
+  saque(titular, valorSaque) {
+    const cliente = this.consultarCliente(titular);
+    if (cliente !== null) {
+      if (cliente.saldo < valorSaque) {
+        console.log("Não foi possível realizar o saque: Saldo insuficiente");
+      } else {
+        cliente.saldo -= valorSaque;
+        console.log("Seu novo saldo é de: R$ " + cliente.saldo.toFixed(2));
+      }
+    } else {
+      console.log("Não foi possível realizar o depósito");
+    }
+  },
+  listarClientes() {
     console.table(banco.clientes);
-  }
-}
-banco.adicionarCliente('01','conta corrente',0,'Fulano 1');
-banco.adicionarCliente('02','conta corrente',0,'Fulano 2');
-banco.adicionarCliente('03','conta corrente',0,'Fulano 3');
-banco.adicionarCliente('04','conta corrente',0,'Lucas');
-const clienteEncontrado = banco.consultarCliente('Lucas');
-console.log(clienteEncontrado);
-banco.deposito('Lucas',50);
+  },
+};
+banco.adicionarCliente("01", "conta corrente", 0, "Fulano 1");
+banco.adicionarCliente("02", "conta corrente", 0, "Fulano 2");
+banco.adicionarCliente("03", "conta corrente", 0, "Fulano 3");
+banco.adicionarCliente("04", "conta corrente", 0, "Lucas");
+banco.deposito("Lucas", 50);
+banco.deposito("Lucas", 300.68);
 banco.listarClientes();
+const n = 2;
 
 //console.table(banco.clientes);
 
-// for of 
-
-
-
+// for of
 
 // const conta1 = new Conta('01','conta corrente',0,'Fulano 1');
 // const conta2 = new Conta('02','conta poupança',0,'Fulano 2');
